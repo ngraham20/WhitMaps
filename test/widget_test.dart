120491c71f2533f930c800082134f60e9ef1e557
Nathaniel Graham <ngraham20@my.whitworth.edu>
2020-03-05 14:54:10 -0800

    minor changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:swe_project/main.dart';
+import 'package:whitmaps/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
