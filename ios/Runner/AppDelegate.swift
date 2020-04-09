15235e5af3aaffb7289ddf7af29935b9cc481571
Kelly <kellyolivier3@gmail.com>
2020-04-09 14:30:11 -0700

    Revert "Attempt #1 at hidding api key"
    This reverts commit a552a6800e153928dfb7df380c497ece2533cc1a.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,5 @@
 import UIKit
 import Flutter
-import GoogleMaps
-
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,14 +7,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let path = Bundle.main.path(forResource: "Keys", ofType: "plist")
-    let plist = NSDictionary(contentsOfFile:path!)
-    let value = plist?.object(forKey: "googleApiKey") as! String
-
-    GMSServices.provideAPIKey(value)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
 }
-
