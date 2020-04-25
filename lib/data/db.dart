78280c947841db04ea2bb54b1c7340186765b57b
Nathaniel Graham <ngraham20@my.whitworth.edu>
2020-04-25 14:50:56 -0700

    directory theoretically will work

--- lib/data/db.dart
+++ lib/data/db.dart
@@ -2,97 +2,100 @@ import 'package:sqflite/sqflite.dart';
 import 'package:whitmaps/models/contact.dart';
 import 'package:whitmaps/models/poi.dart';
 
-createDatabase() async {
-  String databasesPath = await getDatabasesPath();
-  String dbPath = databasesPath + 'my.db';
 
-  var database = await openDatabase(dbPath, version: 1, onCreate: initDB);
-  return database;
+class DB {
+  static createDatabase() async {
+    String databasesPath = await getDatabasesPath();
+    String dbPath = databasesPath + 'my.db';
+
+    var database = await openDatabase(dbPath, version: 1, onCreate: initDB);
+    return database;
+    }
+
+  static void initDB(Database db, int version) async {
+    await db.execute("CREATE TABLE Customer ("
+          "id INTEGER PRIMARY KEY,"
+          "first_name TEXT,"
+          "last_name TEXT,"
+          "email TEXT"
+          ")");
   }
 
-void initDB(Database db, int version) async {
-  await db.execute("CREATE TABLE Customer ("
-        "id INTEGER PRIMARY KEY,"
-        "first_name TEXT,"
-        "last_name TEXT,"
-        "email TEXT"
-        ")");
-}
+  //-----------------------------------------------------------
+  //                       CONTACT
+  //-----------------------------------------------------------
 
-//-----------------------------------------------------------
-//                       CONTACT
-//-----------------------------------------------------------
-
-Future<int> createContact(Database db, Contact contact) async {
-  var result = await db.insert("Contact", contact.toMap());
-  return result;
-}
-
-Future<List> getContacts(Database db) async {
-  var result = await db.query("Contact", columns:
-  ["name", "primary_phone", 
-  "home_phone", "mobile_phone", "office_phone"]);
-  return result.toList();
-}
-
-Future<Contact> getContact(Database db, String name) async {
-  List<Map> results = await db.query("Contact",
-    columns: ["name", "primary_phone", "home_phone", "mobile_phone", "office_phone"],
-    where: 'name = ?',
-    whereArgs: [name]);
-
-  if (results.length > 0) {
-    return new Contact.fromJson(results.first);
+  static Future<int> createContact(Database db, Contact contact) async {
+    var result = await db.insert("Contact", contact.toMap());
+    return result;
   }
 
-  return null;
-}
+  static Future<List> getContacts(Database db) async {
+    var result = await db.query("Contact", columns:
+    ["name", "primary_phone", 
+    "home_phone", "mobile_phone", "office_phone"]);
+    return result.toList();
+  }
 
-Future<int> updateContact(Database db, Contact contact) async {
-  return await db.update("Contact",
-    contact.toMap(),
+  static Future<Contact> getContact(Database db, String name) async {
+    List<Map> results = await db.query("Contact",
+      columns: ["name", "primary_phone", "home_phone", "mobile_phone", "office_phone"],
+      where: 'name = ?',
+      whereArgs: [name]);
+
+    if (results.length > 0) {
+      return new Contact.fromJson(results.first);
+    }
+
+    return null;
+  }
+
+  static Future<int> updateContact(Database db, Contact contact) async {
+    return await db.update("Contact",
+      contact.toMap(),
+      where: 'name = ? and primary_phone = ?',
+      whereArgs: [contact.name, contact.primaryPhone]);
+  }
+
+  static Future<int> deleteContact(Database db, Contact contact) async {
+    return await db.delete("Contact",
     where: 'name = ? and primary_phone = ?',
-    whereArgs: [contact.name, contact.primaryPhone]);
-}
+    whereArgs: [contact.name, contact.primaryPhone]
+    );
+  }
 
-Future<int> deleteContact(Database db, Contact contact) async {
-  return await db.delete("Contact",
-  where: 'name = ? and primary_phone = ?',
-  whereArgs: [contact.name, contact.primaryPhone]
-  );
-}
+  //-----------------------------------------------------------
+  //                          POI
+  //-----------------------------------------------------------
 
-//-----------------------------------------------------------
-//                          POI
-//-----------------------------------------------------------
+  static Future<int> createPoi(Database db, Poi poi) async {
+    var result = await db.insert("Contact", poi.toMap());
+    return result;
+  }
 
-Future<int> createPoi(Database db, Poi poi) async {
-  var result = await db.insert("Contact", poi.toMap());
-  return result;
-}
+  static Future<List> getPoi(Database db) async {
+    var result = await db.query("Poi", columns:
+    ["latitude", "longitude", "name",
+    "description", "floorplanImagePath",
+    "numParkingSpaces"]);
+    return result.toList();
+  }
 
-Future<List> getPoi(Database db) async {
-  var result = await db.query("Poi", columns:
-  ["latitude", "longitude", "name",
-   "description", "floorplanImagePath",
-   "numParkingSpaces"]);
-  return result.toList();
-}
+  static Future<int> updatePoi(Database db, Poi poi) async {
+    return await db.update("Contact",
+      poi.toMap(),
+      where: 'latitude = ? and longitude = ?',
+      whereArgs: [poi.latitude, poi.longitude]);
+  }
 
-Future<int> updatePoi(Database db, Poi poi) async {
-  return await db.update("Contact",
-    poi.toMap(),
+  static Future<int> deletePoi(Database db, Poi poi) async {
+    return await db.delete("Contact",
     where: 'latitude = ? and longitude = ?',
-    whereArgs: [poi.latitude, poi.longitude]);
-}
+    whereArgs: [poi.latitude, poi.longitude]
+    );
+  }
 
-Future<int> deletePoi(Database db, Poi poi) async {
-  return await db.delete("Contact",
-  where: 'latitude = ? and longitude = ?',
-  whereArgs: [poi.latitude, poi.longitude]
-  );
-}
-
-void closeDatabase(Database db) async {
-  await db.close();
+  static void closeDatabase(Database db) async {
+    await db.close();
+  }
 }
---
+++ lib/models/directory.dart
@@ -0,0 +1,47 @@
+import 'dart:collection';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:whitmaps/data/db.dart';
+import 'package:whitmaps/models/contact.dart';
+
+enum Transaction {
+  addContact,
+  removeContact
+}
+
+class DirHistory {
+  final trans;
+  final contact;
+
+  const DirHistory(this.trans, this.contact);
+}
+
+class Directory {
+  List<Contact> contacts;
+  ListQueue<DirHistory> history;
+
+  Directory({this.contacts});
+
+  void addContact(Contact contact) {
+    contacts.add(contact);
+    history.add(DirHistory(Transaction.addContact, contact));
+  }
+
+  void removeContact(Contact contact) {
+    contacts.remove(contact);
+    history.add(DirHistory(Transaction.removeContact, contact));
+  }
+
+  void save() {
+    Database db = DB.createDatabase();
+    while (!history.isEmpty) {
+      var transaction = history.removeFirst();
+      if (transaction.trans == Transaction.addContact) {
+        DB.createContact(db, transaction.contact);
+      }
+      else {
+        DB.deleteContact(db, transaction.contact);
+      }
+    }
+  }
+}
