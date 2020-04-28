0091844308a01427062248d57604a2a3f8674a6b
Kelly <kellyolivier3@gmail.com>
2020-04-28 14:40:26 -0700

    re-fix of tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,14 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:whitmaps/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:whitmaps/main.dart';
 
-// void main() {
-//    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+ void main() {
+    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 //   //   // Build our app and trigger a frame.
-//     await tester.pumpWidget(WhitMaps());
+     await tester.pumpWidget(WhitMaps());
 
 //   //   // Verify that our counter starts at 0.
 //     expect(find.text('WhitMaps Home'), findsOneWidget);
@@ -25,5 +25,5 @@
 //   //   // Verify that our counter has incremented.
 //   //   expect(find.text('0'), findsNothing);
 //   //   expect(find.text('1'), findsOneWidget);
-//    });
-//}
+    });
+}
