b97597456ce55805b17780462279b03eeea5fef7
Kelly <kellyolivier3@gmail.com>
2020-05-04 21:23:41 -0700

    List exists, not yet populated

--- lib/fragments/second_fragment.dart
+++ lib/fragments/second_fragment.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
-class SecondFragment extends StatelessWidget {
+final List<String> entries = <String>['Main Phone', 'Security', 'Facilities'];
+final List<int> colorCodes = <int>[600, 500, 100];
+
+class SecondFragment extends StatefulWidget {
+  @override 
+  SecondFragmentState createState() => SecondFragmentState();
+}
+
+class SecondFragmentState extends State<SecondFragment> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Center(
-      child: new Text("Phone Book"),
+      child: new Scaffold(
+        body: Center(
+          child: ListView.separated(
+          padding: const EdgeInsets.all(8),
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 50,
+              child: Center(child: Text('${entries[index]}')),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) => const Divider(),
+        )
+      ),
+      ),
     );
   }
-
 }
+
+
+
